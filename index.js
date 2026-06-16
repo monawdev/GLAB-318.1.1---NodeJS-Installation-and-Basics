23c8ba75176520fc493e0f242a34cad69cc745fc
منور <125405236+monawdev@users.noreply.github.com>
2026-06-16 16:21:26 -0400

    GLAB 318.1.1 completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Home Page</h1>");
+  }
+
+  else if (req.url === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>About Page</h1><p>This is the about route.</p>");
+  }
+
+  else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>404 Not Found</h1>");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
